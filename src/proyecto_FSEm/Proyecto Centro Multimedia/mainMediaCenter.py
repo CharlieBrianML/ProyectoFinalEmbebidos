38f0016868f0841a31f20b1c43e543c180fbca8c
CharlieBrianML <charls_brian@hotmail.es>
2021-02-05 05:23:02 -0600

    Se ha modificado menu principal

--- src/proyecto_FSEm/Proyecto Centro Multimedia/mainMediaCenter.py
+++ src/proyecto_FSEm/Proyecto Centro Multimedia/mainMediaCenter.py
@@ -1,6 +1,7 @@
 import vlc
 import time
 import glob
+import os
 
 #instancia del reproductor
 vlc_instance = vlc.Instance()
@@ -9,52 +10,60 @@ player = vlc_instance.media_list_player_new()#funcion para hacer slideshow
 
 
 def reproducirUSB():
-	#se guardan los nombres de los archivos tipo png en una lista
-	varPhotoFiles = glob.glob("Fotos/*.png")#En carpeta fotos
-	#se guardan los nombres de los archivos tipo mp4 en una lista
-	varVideoFiles = glob.glob("Videos/*.mp4")#EN carpeta videos
-	#se guardan los nombres de los archivos tipo mp3 en una lista
-	varMusicFiles = glob.glob("Musica/*.mp3")#EN carpeta musica
-	
-	if (varPhotoFiles and varVideoFiles) or (varPhotoFiles and varMusicFiles) or (varVideoFiles and varMusicFiles) :
-		print("Elige que reproducir:\n1.-Fotos\n2.-Videos\n3.-Musica")
-		opcionReproduccion = input()		
-		if opcionReproduccion == '1':
-			mymedia = varPhotoFiles
-			tiempoReproduccion = 4 #tiempo que cada slide está en pantalla
-		elif opcionReproduccion == '2':
-			mymedia = varVideoFiles
-			tiempoReproduccion = 33 #ajustar para cada video	
-		elif opcionReproduccion == '3':
-			mymedia = varMusicFiles
-			tiempoReproduccion = 33 #ajustar para cada cancion	
-	elif not varVideoFiles and not varMusicFiles:#si no hay archivos de video se cargan fotos
-	    mymedia = varPhotoFiles
-	    tiempoReproduccion = 4 #tiempo que cada slide está en pantalla
-	elif not varPhotoFiles and not varMusicFiles:#si no hay archivos de fotos ni musica se cargan videos
-	    mymedia = varVideoFiles
-	    tiempoReproduccion = 33 #ajustar para cada video
-	elif not varPhotoFiles and not varVideoFiles:#si no hay archivos de fotos ni musica se cargan videos
-	    mymedia = varMusicFiles
-	    tiempoReproduccion = 33 #ajustar para cada video
+    #se guardan los nombres de los archivos tipo png en una lista
+    varPhotoFiles = glob.glob("Fotos/*.png")#En carpeta fotos
+    #se guardan los nombres de los archivos tipo mp4 en una lista
+    varVideoFiles = glob.glob("Videos/*.mp4")#EN carpeta videos
+    #se guardan los nombres de los archivos tipo mp3 en una lista
+    varMusicFiles = glob.glob("Musica/*.mp3")#EN carpeta musica
 
-	Media = vlc_instance.media_list_new(mymedia)
-	player.set_media_list(Media)
+    if ((varPhotoFiles and varVideoFiles) or (varPhotoFiles and varMusicFiles) or (varVideoFiles and varMusicFiles)) :
+        print("\nElige que reproducir:\n1.-Fotos\n2.-Videos\n3.-Musica\nOpcion: ", end='')
+        opcionReproduccion = input()
+        print('')
+        if (opcionReproduccion == '1'):
+            mymedia = varPhotoFiles
+            tiempoReproduccion = 4 #tiempo que cada slide esta en pantalla
+        elif (opcionReproduccion == '2'):
+            mymedia = varVideoFiles
+            tiempoReproduccion = 33 #ajustar para cada video
+        elif (opcionReproduccion == '3'):
+            mymedia = varMusicFiles
+            tiempoReproduccion = 5 #ajustar para cada cancion
+    elif (not varVideoFiles and not varMusicFiles):#si no hay archivos de video se cargan fotos
+        mymedia = varPhotoFiles
+        tiempoReproduccion = 4 #tiempo que cada slide esta en pantalla
+    elif (not varPhotoFiles and not varMusicFiles):#si no hay archivos de fotos ni musica se cargan videos
+        mymedia = varVideoFiles
+        tiempoReproduccion = 33 #ajustar para cada video
+    elif (not varPhotoFiles and not varVideoFiles):#si no hay archivos de fotos ni musica se cargan videos
+        mymedia = varMusicFiles
+        tiempoReproduccion = 33 #ajustar para cada video
 
-	#cada elemento de la lista se reproduce en pantalla por 4 segundos
-	for index, name in enumerate(mymedia):
-	    player.play_item_at_index(index)
-	    time.sleep(tiempoReproduccion)#el tiempo de reproduccion de las fotos, videos o musica
-	Media.close()#IMPORTANTE, debe cerrarse el reproductor
+    Media = vlc_instance.media_list_new(mymedia)
+    player.set_media_list(Media)
 
-print("1.- Ver servicio de video online(Netlfix). \n2.- Reproducir musica(Spotify). \n3.- Reproducir contenido en USB")
-opcion = input()
-if opcion == '3':
-	reproducirUSB()
-else:
-	print("Reproduciendo Netlfix o Spotify")
-	print(opcion)
-
-
-
-#Comentario
+    #cada elemento de la lista se reproduce en pantalla por 4 segundos
+    for index, name in enumerate(mymedia):
+        player.play_item_at_index(index)
+        print('Reproduciendo: ',name.split('/')[1])
+        time.sleep(tiempoReproduccion)#el tiempo de reproduccion de las fotos, videos o musica
+    #Media.close()#IMPORTANTE, debe cerrarse el reproductor
+def menu():
+    print("1.- Ver servicio de video online(Netlfix). \n2.- Reproducir musica(Spotify). \n3.- Reproducir contenido en USB\nSalir (Presione cualquier tecla)\nOpcion: ",end='')
+    opcion = input()
+    if (opcion == '1'):
+        #reproducirUSB()
+        os.system('clear')
+        menu()
+    if (opcion == '2'):
+        #reproducirUSB()
+        os.system('clear')
+        menu()
+    if (opcion == '3'):
+        reproducirUSB()
+        os.system('clear')
+        menu()
+    else:
+        exit()
+menu()
